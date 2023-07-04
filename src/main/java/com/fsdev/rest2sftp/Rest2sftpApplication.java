3dde2af7bd81c4606e2b12900d80cce30bdc832d
fsbs2 <gabriel_fsbs2@hotmail.com>
2023-07-04 02:20:25 -0300

    OpenApi spec, new dependencies on pom, swagger codegen plugin

---
+++ src/main/java/com/fsdev/rest2sftp/Rest2sftpApplication.java
@@ -0,0 +1,13 @@
+package com.fsdev.rest2sftp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rest2sftpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Rest2sftpApplication.class);
+    }
+
+}
---
+++ src/test/java/com/fsdev/rest2sftp/Rest2sftpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fsdev.rest2sftp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rest2sftpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
